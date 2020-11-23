a9239c3ee211467925927c170df57226d4fe7c28
goodboy3 <zhangzhengbo@hotmail.com>
2020-11-23 12:04:57 +0800

    bugfix/send heatbeat after gin start

--- terminal/main.go
+++ terminal/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/daqnext/meson-common/common"
+	"github.com/daqnext/meson-common/common/httputils"
 	"github.com/daqnext/meson-common/common/logger"
 	"github.com/daqnext/meson-terminal/terminal/manager/account"
 	"github.com/daqnext/meson-terminal/terminal/manager/config"
@@ -15,6 +16,7 @@ import (
 	"github.com/robfig/cron/v3"
 	"io/ioutil"
 	"math/rand"
+	"strconv"
 	"strings"
 	"time"
 
@@ -26,6 +28,8 @@ import (
 func init() {
 	terminallogger.InitLogger()
 }
+
+var healthPort = 0
 
 func main() {
 	config.CheckConfig()
@@ -76,25 +80,68 @@ func main() {
 		}
 	}
 
-	go func() {
-		time.Sleep(time.Second * 10)
+	//go func() {
+	//	time.Sleep(time.Second * 10)
+	//	statemgr.SendStateToServer()
+	//	//start schedule job- upload terminal state
+	//	startScheduleJob()
+	//}()
+
+	CheckGinStart(func() {
+		logger.Info("Terminal Is Running on Port:" + config.UsingPort)
 		statemgr.SendStateToServer()
 		//start schedule job- upload terminal state
 		startScheduleJob()
-	}()
+	})
 
 	addr := fmt.Sprintf(":%s", config.UsingPort)
-	logger.Info("Terminal Is Running on Port:" + config.UsingPort)
+	//logger.Info("Terminal Is Running on Port:" + config.UsingPort)
 	if config.GetString("apiProto") == "http" {
+		healthPort, _ = strconv.Atoi(config.UsingPort)
 		common.GinRouter.Run(addr) // only in local dev
 	} else {
 		//
+		go func() {
+			port, _ := strconv.Atoi(config.UsingPort)
+			for true {
+				port++
+				if port > 65535 {
+					port = 19080
+				}
+				healthPort = port
+				httpAddr := fmt.Sprintf(":%d", port)
+				err := common.GinRouter.Run(httpAddr)
+				if err != nil {
+					continue
+				}
+			}
+		}()
 		err = common.GinRouter.RunTLS(addr, "./"+crtFileName, "./"+keyFileName)
 		if err != nil {
 			logger.Error("server start error", "err", err)
 		}
 	}
+}
 
+func CheckGinStart(onStart func()) {
+	go func() {
+		for true {
+			time.Sleep(time.Second)
+			header := map[string]string{
+				"Content-Type": "application/json",
+			}
+			url := fmt.Sprintf("http://127.0.0.1:%d/api/testapi/health", healthPort)
+			_, err := httputils.Request("GET", url, nil, header)
+			if err != nil {
+				logger.Debug("health check error", "err", err)
+				continue
+			}
+			if onStart != nil {
+				onStart()
+			}
+			break
+		}
+	}()
 }
 
 func startScheduleJob() {
